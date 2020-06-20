7e0be7b3d1edfb1fcd02b777cee4f57464eade06
selman <CEO@MrBs-MacBook-Pro.local>
2020-06-20 14:41:00 -0400

    ikinci commit

--- src/firstpackage/FirstClass.java
+++ src/firstpackage/FirstClass.java
@@ -3,7 +3,11 @@ package firstpackage;
 public class FirstClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("First commit from local repository");
+		System.out.println("Second commit from local repository");
+		
+		
+		
 
 	}
 
